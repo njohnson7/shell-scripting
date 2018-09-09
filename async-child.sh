a7e152bc69f9944097c335385792ce0e6bf2f71e
Nick Johnson <njohnson7@users.noreply.github.com>
2018-09-08 17:40:02 -0700

    finished book

---
+++ async-child.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'Child: child is running...'
+sleep 5
+echo 'Child: child is done. Exiting.'
---
+++ trap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+trap "echo 'I am ignoring you.'" SIGINT SIGTERM
+
+for i in {1..5}; do
+  echo "Iteration $i of 5"
+  sleep 5
+done
